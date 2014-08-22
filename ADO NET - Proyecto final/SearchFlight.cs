2e93a20a8819edbef982f285ac23ed690f518061
Enrique90m <enrique_90m@hotmail.com>
2014-08-21 20:39:07 -0500

    Class SearchFlight - Agrego atributos y constructor basico

--- ADO NET - Proyecto final/SearchFlight.cs
+++ ADO NET - Proyecto final/SearchFlight.cs
@@ -6,8 +6,21 @@ using System.Threading.Tasks;
 
 namespace ADO_NET___Proyecto_final
 {
-    class SearchFlight : FlightBooking, Flight
+    class SearchFlight : FlightBooking
     {
+        //Atributos
+        string sSource { get; set; }
+        string sDestination { get; set; }
+        int iTimeStart { get; set; }
+        int iTimeEnd { get; set; }
 
+        //Contructores
+        public SearchFlight(string psSource, string psDestination, int piTimeStart, int piTimeEnd)
+        {
+            sSource = psSource;
+            sDestination = psDestination;
+            iTimeStart = piTimeStart;
+            iTimeEnd = piTimeEnd;
+        }
     }
 }
