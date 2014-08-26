2cabcf6c9eb6346eef2c23a1ff022e33861acb25
Enrique90m <enrique_90m@hotmail.com>
2014-08-26 17:59:11 -0500

    Form_FlightPayment - Agrego formulario en blanco

---
+++ ADO NET - Proyecto final/Form_FlightPayment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO_NET___Proyecto_final
+{
+    public partial class Form_FlightPayment : Form
+    {
+        public Form_FlightPayment()
+        {
+            InitializeComponent();
+        }
+    }
+}
